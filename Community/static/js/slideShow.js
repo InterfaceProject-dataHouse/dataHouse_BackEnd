4f0c218441189a7158089b16f4b4821752d9e391
minjung <jungb912@daum.net>
2022-02-18 13:25:15 +0900

    login page 파중복코드 정리

---
+++ Community/static/js/slideShow.js
@@ -0,0 +1,291 @@
+$(document).ready(function(){
+
+            $("#btnMovie").click(function(){
+                $("#movieChart_list").show();
+                $("#movieChart_list_Reser").hide();
+                $("#btn_allView_Movie").attr("href", "/movies/?lt=1&ft=0");
+            });
+
+            $("#btnReserMovie").click(function(){
+                $("#movieChart_list").hide();
+                $("#movieChart_list_Reser").show();
+                $("#btn_allView_Movie").attr("href", "/movies/pre-movies.aspx");
+            });
+
+            var movieChartSwiper = new Swiper("#movieChart_list", {
+
+                slidesPerView: 5,
+                spaceBetween: 32,
+                slidesPerGroup: 5,
+                loopFillGroupWithBlank: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+
+                    prevEl: ".swiper-button-prev",
+                },
+                a11y: {
+                    prevSlideMessage: '이전 슬라이드',
+                    nextSlideMessage: '다음 슬라이드',
+                    slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+                },
+            });
+
+            var movieChartSwiper2 = new Swiper("#movieChart_list_Reser", {
+
+                slidesPerView: 5,
+                spaceBetween: 32,
+                slidesPerGroup: 5,
+                loopFillGroupWithBlank: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+
+                    prevEl: ".swiper-button-prev",
+                },
+                a11y: {
+                    prevSlideMessage: '이전 슬라이드',
+                    nextSlideMessage: '다음 슬라이드',
+                    slideLabelMessopenPopupage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+                },
+            });
+
+            $("#movieChart_list_Reser").hide(); //swiper 랜더링 완료 후 숨기기해야함
+
+            var eventSwiper = new Swiper(".event_list", {
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+                slidesPerView: 3,
+                spaceBetween: 24,
+                slidesPerGroup: 3,
+                loopFillGroupWithBlank: true,
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                a11y: {
+                    prevSlideMessage: '이전 슬라이드',
+                    nextSlideMessage: '다음 슬라이드',
+                    slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+                },
+            });
+
+            if (eventSwiper.autoplay.running) {
+                $('.btn_eventControl').addClass('active');
+            }
+
+            $('.btn_eventControl').on({
+                click: function (e) {
+                    if (eventSwiper.autoplay.running) {
+                        $(this).removeClass('active');
+                        eventSwiper.autoplay.stop();
+                    } else {
+                        $(this).addClass('active');
+                        eventSwiper.autoplay.start();
+                    }
+                }
+            });
+
+            $('.specialHall_list a').on({
+                mouseenter:function(e){
+                    var target = e.target;
+                    var idx = $(target).closest('li').index();
+
+                    var arrimgUrl = ["https://img.cgv.co.kr//Front/Main/2021/1209/16390080561620.png","https://img.cgv.co.kr//Front/Main/2021/1130/16382612659930.png","https://img.cgv.co.kr//Front/Main/2021/1130/16382612660240.png","https://img.cgv.co.kr//Front/Main/2021/1130/16382612660560.png"];
+
+                    $(target).closest('li').addClass('active').siblings('li').removeClass('active');
+                    //$('.specialHall_link').attr('href', '') // 링크 주소 넣을 곳
+                    $('.specialHall_link img').attr('src', arrimgUrl[idx]);
+                    $('.specialHall_link img').attr('alt', $(target).children('strong').text());
+                }
+            });
+
+            $('.movieChartBeScreen_btn_wrap a').on({
+                click:function(e){
+                    var target = e.target;
+                    $(target).addClass('active').parent('h3').siblings().children('a').removeClass('active');
+                }
+            });
+
+            var movieSelectionVideoObj = $('.video_wrap video')[0];
+
+            $('.video_wrap video').on({
+                ended:function(){
+                    $('.btn_movieSelection_playStop').removeClass('active');
+                }
+            })
+            // movieSelectionVideoObj.onended = function(){
+
+            // }
+
+            $('.btn_movieSelection_playStop').on({
+                click:function(){
+                    if(movieSelectionVideoObj.paused){
+                        movieSelectionVideoObj.play();
+                        $(this).addClass('active');
+                    }else{
+                        movieSelectionVideoObj.pause();
+                        $(this).removeClass('active');
+                    }
+                }
+            });
+
+            $('.btn_movieSelection_soundOnOff').on({
+                click:function(){
+                    if(movieSelectionVideoObj.muted){
+                        movieSelectionVideoObj.muted = false;
+                        $(this).addClass('active');
+                    }else{
+                        movieSelectionVideoObj.muted = true;
+                        $(this).removeClass('active');
+                    }
+                }
+            });
+
+            var noticeClientBannerSwiper = new Swiper(".noticeClient_banner_list", {
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction:false,
+                },
+                slidesPerView: 1,
+                spaceBetween: 0,
+                loopFillGroupWithBlank: true,
+            });
+
+            if(noticeClientBannerSwiper.autoplay.running){
+                $('.btn_noticeClientBannerControl').addClass('active');
+            }
+
+            $('.btn_noticeClientBannerControl').on({
+                click:function(e){
+                    if(noticeClientBannerSwiper.autoplay.running){
+                        $(this).removeClass('active');
+                        noticeClientBannerSwiper.autoplay.stop();
+                    }else{
+                        $(this).addClass('active');
+                        noticeClientBannerSwiper.autoplay.start();
+                    }
+                }
+            });
+
+        });
+
+
+
+        $(document).ready(function () {
+        $('.nav_menu > li > h2 > a').on({
+            mouseenter: function (e) {
+                var target = e.target;
+                $(target).parents('.nav_menu').find('.nav_overMenu').slideDown(function () {
+                    $('.nav').addClass('active');
+                });
+            },
+            click: function (e) {
+                var target = e.target;
+                if (!$('.nav').hasClass('active')) {
+                    $(this).trigger('mouseenter');
+                } else {
+                    $('.nav').trigger('mouseleave');
+                }
+            }
+        });
+
+        /********************************************************
+        //서브메뉴 구글 GA Analytics 로그 처리 - 2022.01.12 myilsan
+        ********************************************************/
+        //cgv화이트 메뉴클릭
+        $('.nav > .contents > h1 > a').on({
+             click: function (e) {
+                 gaEventLog('PC_GNB', '홈', '');
+            }
+        });
+
+        //주메뉴 클릭
+        $('.nav_menu > li > h2 > a').on({
+            click: function (e) {
+                gaEventLog('PC_GNB', '주메뉴_' + this.text, '');
+            }
+        });
+
+        //주메뉴 하위메뉴 클릭
+        $('.nav_overMenu > dd > h3 > a').on({
+            click: function (e) {
+                var target = e.target;
+                var parText = $(target).parents('.nav_overMenu').find('dt')[0].innerText;
+                gaEventLog('PC_GNB', parText + '_' + this.text, '');
+            }
+        });
+
+        //하위메뉴 최상위 클릭
+        $(".nav_overMenu > dt > h2 > a").on({
+            click: function (e) {
+                gaEventLog('PC_GNB',this.text + '_' + this.text, '');
+            }
+        });
+
+        //------------------end----------------------- [@,.o]>
+
+        $('.nav').on({
+            mouseleave: function (e) {
+                $(this).find('.nav_overMenu').slideUp(200, function () {
+                    $('.nav').removeClass('active');
+                });
+            }
+        });
+
+        $('.totalSearch_wrap input[type=text]').on({
+            focusin: function () {
+                $('.totalSearch_wrap').addClass("active");
+            }
+        });
+
+        $('.btn_totalSearchAutocomplete_close').on({
+            click: function () {
+                $('.totalSearch_wrap').removeClass("active");
+            },
+            focusout: function (e) {
+                //     $('.totalSearch_wrap').removeClass("active");
+
+            }
+        });
+
+        $(this).on({
+            scroll: function (e) {
+                /* S GNB fixed */
+                var headerOffsetT = $('.header').offset().top;
+                var headerOuterH = $('.header').outerHeight(true);
+                var fixedHeaderPosY = headerOffsetT + headerOuterH;
+                var scrollT = $(this).scrollTop();
+                var scrollL = $(this).scrollLeft();
+
+                if (scrollT >= fixedHeaderPosY) {
+                    $('.nav').addClass('fixed');
+                    $('.fixedBtn_wrap').addClass('topBtn');
+                } else {
+                    $('.nav').removeClass('fixed');
+                    $('.fixedBtn_wrap').removeClass('topBtn');
+                }
+
+                /* S > GNB fixed 좌우 스크롤
+                Description
+                - GNB가 fixed 되었을때 좌우 스크롤 되게 처리
+                */
+                if ($('.nav').hasClass('fixed')) {
+                    $('.nav').css({ 'left': -1 * scrollL })
+                } else {
+                    $('.nav').css({ 'left': 0 })
+                }
+                /* E > GNB fixed 좌우 스크롤 */
+                /* S GNB fixed */
+            }
+        });
+
+        $('.btn_gotoTop').on({
+            click: function () {
+                $('html, body').stop().animate({
+                    scrollTop: '0'
+                }, 400);
+            }
+        });
+
+    });
