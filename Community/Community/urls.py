eb4b8959007a17fd64fa815c9371f738c785602b
Seungjin Park <parkseungjin09@gmail.com>
2022-02-22 13:29:26 +0900

    url 수정, merge 필요없음

--- Community/Community/urls.py
+++ Community/Community/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls'), name='accounts'),
+    path('review/', include('review.urls'), name='review'),
     path('admin/', admin.site.urls),
 ]
