6720ae2b7a5062e7fdca7e5e282a92fc85bf22c4
Seungjin Park <parkseungjin09@gmail.com>
2022-02-22 11:58:04 +0900

    reviewPage 수정

--- Community/main/models.py
+++ Community/main/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Review(models.Model):
-    content = models.TextField('내용')
+    content = models.CharField(max_length=255)
--- Community/main/views.py
+++ Community/main/views.py
@@ -17,12 +17,12 @@ def review2(request):
 def reviewPage(request):
     # return render(request, 'main/reviewPage.html')
     reviews = Review.objects.all()
-    review_cont = {'reviews' : reviews}
+    content = {'reviews' : reviews}
 
-    return render(request, 'main/reviewPage.html', review_cont)
+    return render(request, 'main/reviewPage.html', content)
 
 def createTodo(request):
     input_str = request.POST['todoContent']
-    new_memo = Review(content=input_str)
-    new_memo.save()
+    new_review = Review(content=input_str)
+    new_review.save()
     return HttpResponseRedirect(reverse('main:reviewPage'))
